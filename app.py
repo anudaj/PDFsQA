349b08dcf96d59c0f157e13aff95110059f11ed8
anudaj <anudamcg@gmail.com>
2024-02-04 17:36:46 +0530

    new

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-print("Hello to the World!")
-print("Hello")
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask a question about your documents:") 
+
+    with st.sidebar:
+        st.subheader("Your Documents")
+        st.file_uploader("Upload your PDFs here and click on process")
+        st.button("Process")
+
+if __name__ == '__main__':
+    main()
