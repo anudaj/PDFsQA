a00ab0ce68578afca60507e5e2d7182cb5964e3d
anudaj <anudamcg@gmail.com>
2024-02-06 10:13:49 +0530

    Added 2 comments

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import streamlit as st
 def main():
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
     
+    # Home page details with question input area
     st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask a question about your documents:") 
 
+    # Sidebar to attach relevant pdf document
     with st.sidebar:
         st.subheader("Your Documents")
         st.file_uploader("Upload your PDFs here and click on process")
